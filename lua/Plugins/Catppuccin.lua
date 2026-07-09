7f3471b25ed6f7a1e038fa1c94ab45b33a6e8f2d
hbokhar2 <harrisbokhari@outlook.com>
2026-07-08 21:46:05 -0700

    Swapped to Catppuccin colorscheme.

--- lua/Plugins/Catppuccin.lua
+++ lua/Plugins/Catppuccin.lua
@@ -1,4 +1,24 @@
 return{
+	{ 
+		"catppuccin/nvim", 
+		name = "catppuccin", 
+		lazy = false,
+		priority = 1000,
+		config = function()
+
+			require("catppuccin").setup({
+				flavour = "mocha",
+
+				background = {
+					dark = "mocha",
+				},
+
+				transparent_background = true,
+			})
+
+			vim.cmd("colorscheme catppuccin")
+		end,
+	}
 }
 
 --	{
@@ -103,3 +123,5 @@ return{
 --			end
 --		end
 --	}
+
+
